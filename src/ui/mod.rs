74e38b6bc928d683629458964cfb421eb313d663
atomhand <thessalikon@live.co.uk>
2025-03-19 22:59:06 +0000

    ui captures mouse input

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::input::mouse::MouseWheel;
 
 mod camera;
 mod config_egui;
@@ -12,6 +13,34 @@ impl Plugin for UiPlugin {
             fps_widget::FpsWidgetPlugin,
             config_egui::ConfigEguiPlugin,
             camera::CameraPlugin,
-        ));
+        ))
+        // Egui mouse input culling (see below)
+        .add_systems(
+            PreUpdate, 
+            absorb_egui_inputs
+                .after(bevy_egui::input::write_egui_input_system)
+                .before(bevy_egui::begin_pass_system),
+        );
     }
+}
+
+// Cull mouse inputs pre-frame if Egui has captured the pointer
+
+// Source and useful discussion here https://github.com/vladbat00/bevy_egui/issues/47
+// Not doing anything about keyboard input for now
+//   - Ideally an active textbox should capture the input
+//   - I think that's probably a little more complicated to implement
+
+fn absorb_egui_inputs(
+    mut contexts: bevy_egui::EguiContexts,
+    mut mouse: ResMut<ButtonInput<MouseButton>>,
+    mut mouse_wheel: ResMut<Events<MouseWheel>>,
+) {
+    let ctx = contexts.ctx_mut();
+    if !(ctx.wants_pointer_input() || ctx.is_pointer_over_area()) {
+        return;
+    }
+
+    mouse.reset_all();
+    mouse_wheel.clear();
 }

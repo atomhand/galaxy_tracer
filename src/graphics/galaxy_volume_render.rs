742944780b5413886e39adabbf0054ab01c7fe99
atomhand <thessalikon@live.co.uk>
2025-05-14 22:27:00 +0100

    toggle for whether to render volume to background layer

--- src/graphics/galaxy_volume_render.rs
+++ src/graphics/galaxy_volume_render.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 use bevy::{
     prelude::*,
     reflect::TypePath,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    render::{view::RenderLayers,render_resource::{AsBindGroup, ShaderRef}},
 };
 
 use super::shader_types::*;
@@ -14,7 +14,7 @@ impl Plugin for GalaxyVolumePlugin {
         app.add_plugins(MaterialPlugin::<GalaxyVolumeMaterial>::default());
 
         app.add_systems(Startup, setup_galaxy_volume)
-            .add_systems(Update, update_volume_material);
+            .add_systems(Update, (update_volume_material,update_galaxy_volume));
     }
 }
 
@@ -38,6 +38,23 @@ fn setup_galaxy_volume(
         volume_upscaler::background_render_layer(),
         bevy::render::view::NoFrustumCulling,
     ));
+}
+
+fn update_galaxy_volume(
+    mut commands: Commands,
+    query : Query<Entity,With<GalaxyVolume>>,
+    galaxy_config: Res<GalaxyConfig>
+) {
+    // TODO: we dont have to do this every frame
+    if let Ok(entity) = query.single() {
+        commands.entity(entity).insert(
+            if galaxy_config.draw_volume_to_background {
+                volume_upscaler::background_render_layer()
+            } else {
+                RenderLayers::layer(0)
+            }
+        );
+    }
 }
 
 fn update_volume_material(
